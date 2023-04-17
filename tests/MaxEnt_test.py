88faeeeb5e1762e305403d8a527c22c4c4287abd
Agnes Bi <67855661+AgnesBi@users.noreply.github.com>
2023-04-17 16:52:47 -0400

    MaxEnt bug fixed
    MaxEnt class can now handles tableaux with multiple winners.

--- tests/MaxEnt_test.py
+++ tests/MaxEnt_test.py
@@ -1,3 +1,5 @@
+# Last updated: 04/17/2023
+
 import sys
 import os
 from pathlib import Path
@@ -9,26 +11,65 @@ module_dir = os.path.join(Path(__file__).parent.parent, "utils")
 sys.path.append(module_dir)
 
 import pytest
-import MaxEnt as MaxEntClass
+import MaxEnt as MaxEntUtil
 
 from collections import namedtuple
 import numpy as np
+import math
 
-Dataset = namedtuple('Dataset', ['constraints', 'weights', 'model'])
+Dataset = namedtuple("Dataset", ["constraints", "weights"])
+
 
 @pytest.fixture
-def dataset_1():
+def ex_const_1():
     constraints = ["M1", "M2", "F"]
     weights = [7, 10, 0]
-    model = MaxEntClass.MaxEnt(constraints, weights)
-    return Dataset(constraints, weights, model)
-
-def test_accessors(dataset_1):
-    assert dataset_1.constraints == dataset_1.model.cns
-    assert dataset_1.weights == dataset_1.model.cws
+    return Dataset(constraints, weights)
 
 
-def test_sorted_by_weights(dataset_1):
-    output = dataset_1.model.sorted_by_weights()
+@pytest.fixture
+def ex_vio_1():
+    return [
+        np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
+        np.array([[1, 1, 0], [0, 1, 1]]),
+    ]
+
+
+@pytest.fixture
+def ex_observed_prob_1():
+    return [np.array([[1 / 3, 2 / 3, 0]]), np.array([[0, 1]])]
+
+
+def test_compute_probabilities(ex_const_1, ex_vio_1):
+    output = MaxEntUtil.compute_probabilities(ex_const_1.weights, ex_vio_1)
+
+    exp0 = np.array([math.exp(-7), math.exp(-10), math.exp(0)])
+    mar0 = math.exp(-7) + math.exp(-10) + math.exp(0)
+    pred0 = exp0 / mar0
+    assert output[0].any() == pred0.any()
+
+    exp1 = np.array([math.exp(-17), math.exp(-7)])
+    mar1 = math.exp(-17) + math.exp(-7)
+    pred1 = exp1 / mar1
+    assert output[1].any() == pred1.any()
+
+
+def test_get_weighted_winner_violations(ex_observed_prob_1, ex_vio_1):
+    output = MaxEntUtil.get_weighted_winner_violations(ex_observed_prob_1, ex_vio_1)
+    expected = [np.array([[1 / 3, 2 / 3, 0]]), np.array([[0, 1, 1]])]
+    assert output[0].any() == expected[0].any()
+    assert output[1].any() == expected[1].any()
+
+
+def test_accessors(ex_const_1):
+    model = MaxEntUtil.MaxEnt(ex_const_1.constraints, ex_const_1.weights)
+
+    assert ex_const_1.constraints == model.cns
+    assert ex_const_1.weights == model.cws
+
+
+def test_sorted_by_weights(ex_const_1):
+    model = MaxEntUtil.MaxEnt(ex_const_1.constraints, ex_const_1.weights)
+    output = model.sorted_by_weights()
     expected = [("M2", 10), ("M1", 7), ("F", 0)]
     assert output == expected
--- utils/MaxEnt.py
+++ utils/MaxEnt.py
@@ -5,11 +5,63 @@
 #   Step 2: Compares it to the given probability distribution;
 #   Step 3: Updates the weights
 #       if stochastic Gradient Descent (SGD): by going in the direction towards the minimum of the objective function.
-# Last updated: 04/11/2023
+#
+# Last updated: 04/17/2023
 
 import numpy as np
+import random
 
 
+# ==================== Helper functions =========================================
+#
+def compute_probabilities(weights, violations):
+    """Predicts probabilities for a particular violation profile.
+
+    Args:
+        weights (list[float]): Constraint weights.
+        violations (list[np.ndarray[int]]): the violation profiles of a list of tableaux.
+
+    Returns:
+        list[np.array[float]]: predicts probability for each candidate of given URs.
+    """
+    predictions = []
+
+    for vio in violations:
+        harmonies = np.dot(vio, weights)
+        exp = np.exp(-harmonies)
+        marginalization = np.sum(exp, axis=0)
+        prob = exp / marginalization
+        predictions.append(prob)
+
+    return predictions
+
+
+def get_weighted_winner_violations(observed_prob, violations):
+    """_summary_
+
+    Args:
+        observed_prob (list[np.ndarray[float]]): probability of candidates observed for the given URs.
+        violations (list[np.ndarray[int]]): the violation profiles of a list of tableaux.
+    Returns:
+        list[np.ndarray[float]]: the (weighted) violation profile of the winner candidates.
+    """
+    if len(observed_prob) != len(violations):
+        raise ValueError(
+            "Input error: the length of the two input lists should be the same."
+        )
+
+    rst = []
+    for i in range(len(observed_prob)):
+        weighted_tab = observed_prob[i].T * violations[i]
+        # Removes the rows with only 0's (i.e., loser candidates).
+        rst.append(weighted_tab[~np.all(weighted_tab == 0, axis=1)])
+
+    return rst
+
+
+#
+# ==================== MaxEnt class =========================================
+#
 class MaxEnt:
     """A general-purpose MaxEnt class.
 
@@ -19,111 +71,88 @@ class MaxEnt:
 
     Methods:
         sorted_by_weights: Returns sorted (constraint, weight) tuples.
-        compute_probabilities: Returns the predicted probability distribution for any given violation profile.
+        SGD_learn:
     """
 
     def __init__(self, constraints, weights):
         self._cns = constraints
         self._cws = weights
-        self._rng = np.random.default_rng()
 
     # TODO: figure out how we want the string representation to be.
     def __repr__(self):
         pass
 
-
     @property
     def cns(self):
-        """Accesses constraint names.
-        """
+        """Accesses constraint names."""
         return self._cns
-    
-    
+
     @property
     def cws(self):
-        """Accesses constraint weights.
-        """
+        """Accesses constraint weights."""
         return self._cws
 
-
-    @property
-    def rng(self):
-        """Accesses random generator.
-        """
-        return self._rng
-
-    # TODO: write a unit test for this; and whether we need this as a class method.
     def sorted_by_weights(self):
-        """Displays constraints from highest to lowest weights.
+        """Sorts constraints from the highest- to lowest-weighted.
 
         Returns:
             list[tuple[str, float]]: a list of (constraint, weight) tuple sorted from highest to lowest weighted.
         """
-        cs_sorted = sorted(zip(self.cns, self.cws), key=lambda x: x[1], reverse=True)
-        return cs_sorted
+        return sorted(zip(self.cns, self.cws), key=lambda x: x[1], reverse=True)
 
-    def compute_probabilities(self, violations):
-        """Predicts probabilities for a particular violation profile.
-
-        Args:
-            violations (np.array[np.array[int]]): the violation profiles of a list of tableaux.
-
-        Returns:
-            np.array[np.array[float]]: predicts probability for each candidate of given URs.
-        """
-        harmonies = np.dot(violations, self.w)
-        exp = np.exp(-harmonies)
-        marginalization = np.sum(exp, axis=0)
-        return exp / marginalization
-
-    def SGD_learn(self, candidates, violations, srs, *regs, batch_size=1, iters=10000, eta=0.05):
-        """Updates constraint weights using SGD.
+    def SGD_learn(
+        self, violations, observed_prob, batch_size=1, iters=10000, eta=0.05, *regs
+    ):
+        """Updates constraint weights using SGD, with the option of Mini-batch SGD.
 
         Args:
-            candidates (list[np.array[str]]): candidates considered for each UR.
-            violations (list[np.array[int]]): the violation profiles of a list of tableaux.
-            srs (list[np.array[float]]): probability of a surface form observed for a given UR.
-            *regs: a variable number of regularization terms. Can be None.
+            violations (list[np.ndarray[int]]): the violation profiles of a list of tableaux.
+            observed_prob (list[np.ndarray[float]]): probability of candidates observed for the given URs.
             batch_size (int): batch size. Defaults to 1.
             iters (int): how many iterations to perform. Defaults to 10000.
             eta (float): learning rate. Defaults to 0.05.
+            *regs: a variable number of regularization terms. Passed in as a tuple. Can be None.
 
         Returns:
             _type_: _description_
         """
         # Initialization: begins with the weights that are currently stored.
-        w = self.w
+        weights = self.cws
 
         # Keeps a history of the change in weights.
-        history = [w.squeeze()]
+        history = [weights.squeeze()]
 
-        tableau_n = len(violations)
-        
-        # Passes through all the training data in each iteration,
+        # Calculates the total number of tableaux in the dataset.
+        tableaux_count = len(violations)
+
+        # Sets the number of iterations.
         for _ in range(iters):
-            # Generates a random array of indices of the shape (1, length |URs|).
-            training_order = self.rng.choice(a=range(tableau_n), size=tableau_n, replace=False).to_list
+            # Generates a list of random indices of batch_size.
+            training_set = random.sample(range(tableaux_count), batch_size)
 
+            # Tracks gradient.
             dL_dw = 0
 
-            # Loops through each tableau in the dataset by random order.
-            for t in training_order:
+            for i in training_set:
+                winner_indication_col = observed_prob[i]
+                vio = violations[i]
+
                 # Skips the tableaux without a winning candidate.
-                if srs[t].count(None) == len(srs[t]):
+                if not winner_indication_col.any():
                     continue
-                
-                #TODO: preprocess srs.
-                # Violation profile of the winning candidate(s).
-                winner_violations = np.zeros(self.const.size)
-                probabilities = srs[t]
-                tableau = violations[t]
-                winner_violations = (probabilities * tableau).sum(axis=0)
+
+                # Extracts the violation profile of the winning candidate(s).
+                # Note there might be more than one winning candidate in the case of variation.
+                winner_violations = get_weighted_winner_violations(
+                    winner_indication_col, vio
+                )
+
                 # Computes probability for each candidate given the current weights.
-                P = self.compute_probabilities(violations[t], w)
+                P = compute_probabilities(weights, vio)
 
                 # Computes the gradient.
                 dL_dw += np.transpose(
-                    winner_violations - (violations[t] * P).sum(axis=0, keepdims=True)
+                    winner_violations - (vio * P).sum(axis=0, keepdims=True)
                 )
 
                 # Incorporates regularization if specified.
